8b3dd46b6ac58311151f5327ce2ed1f8a5a27f01
KRofLife <05410kira@gmail.com>
2022-11-01 23:46:21 +0900

    styled-jsxの記述

--- Chapter05/next_app/pages/index.js
+++ Chapter05/next_app/pages/index.js
@@ -5,26 +5,6 @@ export default function Home() {
   let title ="Next.js page"
   let message ="React Next.js sample page."
 
-  const h1 = {
-    textAlign:'right',
-    padding:'5px 15px'
-  }
-
-  const p = {
-    textAlign:'left',
-    margin:'0px 5px',
-    color:'#669',
-    fontSize:'18pt'
-  }
-
-  const subtitle = {
-    textAlign:'center',
-    margin:'0px 5px',
-    color:'#99d',
-    fontSize:'24pt',
-    fontWeight:'bold'
-  }
-
   return (
     <div>
       <Head>
@@ -32,12 +12,32 @@ export default function Home() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           crossOrigin="anonymous"></link>
       </Head>
+      <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+
+        h2 {
+          text-align: center;
+          margin: 0px 5px;
+          color: #aad;
+          font-size: 36pt;
+          font-weight: bold;
+        }
+
+        p {
+          text-align: left;
+          margin: 0px 5px;
+          color: blue;
+          fontSize: 18pt;
+        }
+      `}</style>
 
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <h4 className="my-3" style={subtitle} id="subtitle">{title}</h4>
+        <h2 className="my-3 subtitle">{title}</h2>
         <div className="alert alert-primary text-center">
-          <p className="h5" style={p}>{message}.</p>
+          <p className="h5">{message}.</p>
         </div>
       </div>
     </div>
