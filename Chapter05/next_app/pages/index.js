77bc138fe612f73cc54bf6ae17204e77dfcce8ba
KRofLife <05410kira@gmail.com>
2022-11-03 01:16:36 +0900

    ページでレイアウトを利用

--- Chapter05/next_app/pages/index.js
+++ Chapter05/next_app/pages/index.js
@@ -1,23 +1,19 @@
-import Header from './header'
+import Layout from '../components/layout'
 import Link from 'next/link'
 
 export default function Home() {
-  const title = "Index"
-
-
   return (
     <div>
-      <Header title={title} />
-      <h1 className="bg-primary px-3 text-white display-4">React</h1>
-      <div className="container">
-        <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これは、ページ移動のサンプルです。</p>
-          <Link legacyBehavior href="/other">
-            <a>Go to Other page &gt;&gt;</a>
-          </Link>
-        </div>
+      <Layout header="Next.js" title="Top page.">
+      <div className="alert alert-primary text-center">
+        <h5 className="mb-4">Welcome to next.js!</h5>
+        <Link legacyBehavior href="/other">
+          <button className="btn btn-primary">
+            go to Other &gt;&gt;
+          </button>
+        </Link>
       </div>
+      </Layout>
     </div>
   )
 }
