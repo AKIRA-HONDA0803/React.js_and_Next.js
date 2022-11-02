2990126bd123e86445af08af008df86d829c2462
KRofLife <05410kira@gmail.com>
2022-11-03 01:32:51 +0900

    imageコンポーネント

--- Chapter05/next_app/pages/index.js
+++ Chapter05/next_app/pages/index.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layout'
 import Link from 'next/link'
+import MyImage from '../components/image'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
       <div className="alert alert-primary text-center">
         <h5 className="mb-4">Welcome to next.js!</h5>
+        <MyImage fname="image.jpg" size="300" />
         <Link legacyBehavior href="/other">
           <button className="btn btn-primary">
             go to Other &gt;&gt;
