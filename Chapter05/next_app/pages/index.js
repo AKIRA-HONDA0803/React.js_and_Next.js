c7c5cf9ce495ec2c81c45b35f3dcb19d83dcc934
KRofLife <05410kira@gmail.com>
2022-11-03 10:16:03 +0900

    useSWRでステートを用意する

--- Chapter05/next_app/pages/index.js
+++ Chapter05/next_app/pages/index.js
@@ -2,32 +2,38 @@ import Layout from '../components/layout'
 import Link from 'next/link'
 import MyImage from '../components/image'
 import { useState } from 'react'
+import useSWR from 'swr'
 
 export default function Home() {
-  const url ='./data.json'
-  const [data, setData] = useState({message:'', data:[]})
+  // const url ='./data.json'
+  // const [data, setData] = useState({message:'', data:[]})
 
-  fetch(url)
-    .then(res=> res.json())
-    .then(res=> setData(res))
+  // fetch(url)
+  //   .then(res=> res.json())
+  //   .then(res=> setData(res))
+  const { data } = useSWR('/data.json')
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
       <div className="alert alert-primary text-center">
-        <h5 className="mb-4">{data.message}</h5>
+        <h5 className="mb-4">
+          {/* {data.message} */}
+          {data != undefined ? data.message : 'error...' }
+          </h5>
         <table className="table bg-white">
           <thead className="table-dark">
             <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
           </thead>
           <tbody>
-            {data.data.map((value, key)=> (
+            {/* {data.data.map((value, key)=> ( */}
+            {data != undefined ? data.data.map((value, key)=> (
               <tr key={key}>
                 <th>{value.name}</th>
                 <td>{value.mail}</td>
                 <td>{value.age}</td>
               </tr>
-            ))}
+            )) : <tr><th></th><td>no data.</td><td></td></tr>}
           </tbody>
         </table>
         {/* <MyImage fname="image.jpg" size="300" />
